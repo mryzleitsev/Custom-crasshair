b6120839b168748fe10407fbdb624f6d9987fa9f
X1atle <myron.ryzleitsev@gmail.com>
2023-10-29 02:38:24 +0200

    Added crosshairStyle class

---
+++ Custom-crasshair/CrosshairStyle.cs
@@ -0,0 +1,18 @@
+﻿namespace Custom_crasshair;
+
+public class CrosshairStyle
+{
+    private string name;
+    private byte formId;
+    public byte FormId
+    {
+        get { return formId; }
+        set { formId = value; }
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+        
+}
